8e7ddf787a6a502d123b96567106c9edeaa57f4e
Dharmaraj <dharmaraj.techie@gmail.com>
2020-04-20 11:15:42 +0530

    add onClickListener

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener {
+            showDie()
+        }
+    }
+
+    private fun showDie() {
+        val resultTextView: TextView = findViewById(R.id.result_tv)
+        val random_number = Random().nextInt(6) + 1
+//        resultTextView.setText(random_number.toString())
+        resultTextView.text = random_number.toString()
     }
 }
